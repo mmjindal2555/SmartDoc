543844ce982effec49be389b3b30f2a88676667b
mmjindal2555 <mmjindal2555@gmail.com>
2017-04-22 19:52:57 +0530

    Static login page.

--- app/src/main/java/com/silk/smartdoc/View/LoginActivity.java
+++ app/src/main/java/com/silk/smartdoc/View/LoginActivity.java
@@ -1,11 +1,13 @@
 package com.silk.smartdoc.View;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -17,11 +19,11 @@ import com.silk.smartdoc.R;
 
 public class LoginActivity extends Activity {
 
-    TextView sdlogo;
+    TextView sdLogo;
     EditText usernameET;
     EditText passwordET;
     Button loginButton;
-    Button signupButton;
+    Button signUpButton;
 
     LoggingController loggingController;
 
@@ -30,20 +32,37 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        sdlogo = (TextView)findViewById(R.id.sdLogo);
+        sdLogo = (TextView)findViewById(R.id.sdLogo);
         usernameET = (EditText)findViewById(R.id.usernameEditText);
         passwordET = (EditText)findViewById(R.id.passwordEditText);
         loginButton = (Button)findViewById(R.id.loginButton);
-        signupButton = (Button)findViewById(R.id.signUpButton);
+        signUpButton = (Button)findViewById(R.id.signUpButton);
 
         Typeface myCustomFont = Typeface.createFromAsset(getAssets(), "font/Satisfy-Regular.ttf");
-        sdlogo.setTypeface(myCustomFont);
+        sdLogo.setTypeface(myCustomFont);
         getWindow().setStatusBarColor(getResources().getColor(R.color.statusbarcolor));
         // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-        myRef.setValue("Hello, World!");
+        //FirebaseDatabase database = FirebaseDatabase.getInstance();
+        //DatabaseReference myRef = database.getReference("message");
+        //myRef.setValue("Hello, World!");
 
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (usernameET.getText().toString().equals("a") && passwordET.getText().toString().equals("b")) {
+                    Intent intent = new Intent(LoginActivity.this, HealthForum.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,SignUp.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
