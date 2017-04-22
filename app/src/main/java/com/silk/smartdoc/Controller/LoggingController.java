6c6d757970e14d990a494c4823348815892d7d67
mmjindal2555 <mmjindal2555@gmail.com>
2017-04-22 19:05:15 +0530

    Firebase added to gradle. Created dirs.

---
+++ app/src/main/java/com/silk/smartdoc/Controller/LoggingController.java
@@ -0,0 +1,8 @@
+package com.silk.smartdoc.Controller;
+
+/**
+ * Created by Manish on 4/22/2017.
+ */
+public class LoggingController {
+
+}
--- app/src/main/java/com/silk/smartdoc/View/LoginActivity.java
+++ app/src/main/java/com/silk/smartdoc/View/LoginActivity.java
@@ -6,20 +6,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.silk.smartdoc.Controller.LoggingController;
 import com.silk.smartdoc.R;
 
 public class LoginActivity extends Activity {
 
     TextView sdlogo;
+    EditText usernameET;
+    EditText passwordET;
+    Button loginButton;
+    Button signupButton;
+
+    LoggingController loggingController;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         sdlogo = (TextView)findViewById(R.id.sdLogo);
+        usernameET = (EditText)findViewById(R.id.usernameEditText);
+        passwordET = (EditText)findViewById(R.id.passwordEditText);
+        loginButton = (Button)findViewById(R.id.loginButton);
+        signupButton = (Button)findViewById(R.id.signUpButton);
+
         Typeface myCustomFont = Typeface.createFromAsset(getAssets(), "font/Satisfy-Regular.ttf");
         sdlogo.setTypeface(myCustomFont);
         getWindow().setStatusBarColor(getResources().getColor(R.color.statusbarcolor));
@@ -28,6 +44,8 @@ public class LoginActivity extends Activity {
         DatabaseReference myRef = database.getReference("message");
 
         myRef.setValue("Hello, World!");
+
+
 
     }
 }
