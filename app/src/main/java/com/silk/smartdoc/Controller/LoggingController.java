f6eda10c3bcc80fc0e5a9b6d242e72e2631c7e12
akash agarwal <aka241193@gmail.com>
2017-04-23 15:48:02 +0530

    Added NavDrawer.

--- app/src/main/java/com/silk/smartdoc/Controller/LoggingController.java
+++ app/src/main/java/com/silk/smartdoc/Controller/LoggingController.java
@@ -1,8 +1,16 @@
 package com.silk.smartdoc.Controller;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.view.View;
+
+import com.silk.smartdoc.View.HealthForum;
+import com.silk.smartdoc.View.LoginActivity;
+
 /**
  * Created by Manish on 4/22/2017.
  */
 public class LoggingController {
 
+    
 }
--- app/src/main/java/com/silk/smartdoc/View/HealthForum.java
+++ app/src/main/java/com/silk/smartdoc/View/HealthForum.java
@@ -1,24 +1,69 @@
 package com.silk.smartdoc.View;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.silk.smartdoc.R;
+import com.silk.smartdoc.View.MedicineSearch;
+import com.silk.smartdoc.View.TestSearchActivity;
 
-public class HealthForum extends AppCompatActivity {
+public class HealthForum extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_health_forum);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    public void openPostQueryActivity(View view){
+        Intent intent = new Intent(this,PostQueryExperience.class);
+        startActivity(intent);
+    }
+
+    public void openAnswerQueryActivity(View view){
+        Intent intent = new Intent(this,AnswerQuery.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_health_forum, menu);
+        getMenuInflater().inflate(R.menu.health_forum, menu);
         return true;
     }
 
@@ -35,5 +80,33 @@ public class HealthForum extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.health_forum) {
+            Intent intents = new Intent(this,HealthForum.class);
+            startActivity(intents);
+        } else if (id == R.id.medical_tests) {
+            Intent intents = new Intent(this,TestSearchActivity.class);
+            startActivity(intents);
+        } else if (id == R.id.medicines) {
+            Intent intents = new Intent(this,MedicineSearch.class);
+            startActivity(intents);
+        } else if (id == R.id.my_questions) {
+            Intent intents = new Intent(this,HealthForum.class);
+            startActivity(intents);
+        } else if (id == R.id.my_experiences) {
+            Intent intents = new Intent(this,HealthForum.class);
+            startActivity(intents);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
