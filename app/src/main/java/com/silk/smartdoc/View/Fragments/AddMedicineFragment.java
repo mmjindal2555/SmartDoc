52489a55a139e6f45bf62403828b0a8407a8ce18
mmjindal2555 <mmjindal2555@gmail.com>
2017-04-28 21:11:45 +0530

    AdminControl addmedicine v 0.1

--- app/src/main/java/com/silk/smartdoc/View/Fragments/AddMedicineFragment.java
+++ app/src/main/java/com/silk/smartdoc/View/Fragments/AddMedicineFragment.java
@@ -8,11 +8,13 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import com.firebase.client.FirebaseError;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
-import com.firebase.client.ValueEventListener;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 
 import com.silk.smartdoc.Model.Medicine;
 import com.silk.smartdoc.R;
@@ -22,6 +24,8 @@ import com.silk.smartdoc.View.MedicineSearch;
 import org.w3c.dom.Comment;
 
 import java.util.ArrayList;
+
+import static com.facebook.FacebookSdk.getApplicationContext;
 
 /**
  * Created by Manish on 4/27/2017.
@@ -37,29 +41,31 @@ public class AddMedicineFragment extends Fragment{
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.add_medicine_fragment, container, false);
         chemicalsSpinner = (Spinner)view.findViewById(R.id.chemicalsSpinner);
-        return view;
-    }
 
-    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("Chemicals");
-    chemArrayList = new ArrayList<Medicine>();
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("Chemicals");
+        chemArrayList = new ArrayList<String>();
         databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
 
-        @Override
-        public void onDataChange(DataSnapshot dataSnapshot) {
-            Iterable<DataSnapshot> children= dataSnapshot.getChildren();
-            for (DataSnapshot child: children) {
-                String medName = child.getValue(String.class);
-                medArrayList.add(medName);
+            @Override
+            public void onDataChange(com.google.firebase.database.DataSnapshot dataSnapshot) {
+                Iterable<DataSnapshot> children= dataSnapshot.getChildren();
+                for (DataSnapshot child: children) {
+                    String medName = child.child("name").getValue(String.class);
+                    if(medName!=null)
+                    chemArrayList.add(medName);
+                }
+                chemicalsSpinner.setAdapter(new ArrayAdapter<String>(getActivity(),
+                        android.R.layout.simple_spinner_item,chemArrayList));
+
             }
-            chemicalsSpinner.setAdapter(new ArrayAdapter<String>(AddMedicineFragment.this, android.R.layout.simple_list_item_1, medArrayList));
-        }
 
-        @Override
-        public void onCancelled(DatabaseError databaseError) {
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
 
-        }
-    });
-    });
+            }
+        });
+        return view;
+    }
 
 
 }
