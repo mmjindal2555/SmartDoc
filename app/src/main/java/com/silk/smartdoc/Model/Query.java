c05941d8da97581c2aa8ce34142e76b8b16cde63
akash agarwal <aka241193@gmail.com>
2017-05-19 19:15:39 +0530

    Health_Forum_beginning
    Health_Forum_beginning

--- app/src/main/java/com/silk/smartdoc/Model/Query.java
+++ app/src/main/java/com/silk/smartdoc/Model/Query.java
@@ -1,12 +1,19 @@
 package com.silk.smartdoc.Model;
 
+import java.util.ArrayList;
+
 /**
  * Created by dodobhoot on 4/22/2017.
  */
 public class Query {
+    Statement question;
+    ArrayList<Statement> answer;
+    ArrayList<String> tags;
+    String id;
+    /*
     private String tags[];
     private Question question;
     private Answer answers[];
     private boolean isAnswered;
-    private Boolean isExperience;
+    private Boolean isExperience;*/
 }
