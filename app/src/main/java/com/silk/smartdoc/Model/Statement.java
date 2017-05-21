f2f51fb235b7d12082f47d8743f0e2017bf3d778
akash agarwal <aka241193@gmail.com>
2017-05-21 17:21:55 +0530

    Answer Query - Part 1

--- app/src/main/java/com/silk/smartdoc/Model/Statement.java
+++ app/src/main/java/com/silk/smartdoc/Model/Statement.java
@@ -21,4 +21,44 @@ public class Statement {
         this.timestamp=timestamp;
         this.votes=votes;
     }
+    public Statement(){}
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getStatement() {
+        return statement;
+    }
+
+    public void setStatement(String statement) {
+        this.statement = statement;
+    }
+
+    public ArrayList<String> getVotes() {
+        return votes;
+    }
+
+    public void setVotes(ArrayList<String> votes) {
+        this.votes = votes;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
 }
--- app/src/main/java/com/silk/smartdoc/View/AnswerQuery.java
+++ app/src/main/java/com/silk/smartdoc/View/AnswerQuery.java
@@ -2,10 +2,21 @@ package com.silk.smartdoc.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.silk.smartdoc.Model.KeyPairBoolData;
 import com.silk.smartdoc.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AnswerQuery extends AppCompatActivity {
 
@@ -13,6 +24,66 @@ public class AnswerQuery extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_answer_query);
+        final List<String> list = new ArrayList<String>();
+        DatabaseReference databaseReferenceMed = FirebaseDatabase.getInstance().getReference().child("Tags");
+
+        databaseReferenceMed.addListenerForSingleValueEvent(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Iterable<DataSnapshot> children = dataSnapshot.getChildren();
+                for (DataSnapshot child : children) {
+
+                    String medName = child.getValue(String.class);
+                    if (medName != null)
+                        list.add(medName);
+                }
+
+                final List<KeyPairBoolData> listArray1 = new ArrayList<>();
+
+                for (int i = 0; i < list.size(); i++) {
+                    KeyPairBoolData h = new KeyPairBoolData();
+                    h.setId(i + 1);
+                    h.setName(list.get(i));
+                    h.setSelected(false);
+                    listArray1.add(h);
+                }
+                MultiSpinnerSearch searchMultiSpinnerLimit = (MultiSpinnerSearch) findViewById(R.id.searchMultiSpinnerLimit);
+
+
+                /***
+                 * -1 is no by default selection
+                 * 0 to length will select corresponding values
+                 */
+                searchMultiSpinnerLimit.setItems(listArray1, -1, new SpinnerListener() {
+                    ArrayList<String> tags = new ArrayList<String>();
+
+                    @Override
+                    public void onItemsSelected(List<KeyPairBoolData> items) {
+
+
+                        for (int i = 0; i < items.size(); i++) {
+                            if (items.get(i).isSelected()) {
+                                tags.add(items.get(i).getName());
+                            }
+                        }
+                    }
+                });
+
+
+                searchMultiSpinnerLimit.setLimit(4, new MultiSpinnerSearch.LimitExceedListener() {
+                    @Override
+                    public void onLimitListener(KeyPairBoolData data) {
+                        Toast.makeText(getApplicationContext(),
+                                "Limit exceed ", Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @Override
